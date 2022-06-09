b9adc4f577241d9aba2977561cab3141da564c6d
KING@KING143 <56799164+ThyagarajNC@users.noreply.github.com>
2022-06-09 12:19:16 +0530

    Update compoundinterest.py

--- compoundinterest.py
+++ compoundinterest.py
@@ -1,22 +1,17 @@
-def compound_interest(principal, rate, years):
-    return principal*(1 + rate)**years
-
-def compound_interest_recursive(principal, rate, years):
-    if years == 0:
-        return principle
-    else:
-        return 
-
-
-
-principal_str = input("Enter the principal ")
-principal = int(principal_str)
-
-rate_float = input("Enter the interest rate ")
-rate = float(rate_float)
-
-years_str = input("Enter the number of years ")
-years = int(years_str)
-
-print("Principal after", years,"year(s) is",compound_interest\
-      (principal, rate, years))
+from matplotlib import pyplot as pt
+def compoundint(p,r,t):
+    lst=[]
+    x=[]
+    for i in range(1,n+1):
+        x.append(i)
+        a=p*(1+(r/100))**(i*t)
+        ci=a-p
+        lst.append(ci)
+    pt.plot(x,lst)
+    return ci
+    
+p = input("Enter the principal ")
+r= input("Enter the interest rate ")
+t= input("Enter the number of years ")
+n=input("enter the compund interest")
+print("compound interest is",compoundint(p,r,t))
